bfccb395f71a1a6e09cdd8abeb4645c31d177e93
Anton udén <udenanton@hotmail.com>
2023-02-25 15:57:43 +0100

    Update NovaCaptureTheFlag.java

--- src/net/novauniverse/capturetheflag/NovaCaptureTheFlag.java
+++ src/net/novauniverse/capturetheflag/NovaCaptureTheFlag.java
@@ -64,8 +64,8 @@ public class NovaCaptureTheFlag extends JavaPlugin implements Listener {
 		File worldFolder = new File(this.getDataFolder().getPath() + File.separator + "Worlds");
 
 		if (NovaCoreGameEngine.getInstance().getRequestedGameDataDirectory() != null) {
-			mapFolder = new File(NovaCoreGameEngine.getInstance().getRequestedGameDataDirectory().getAbsolutePath() + File.separator + getName() + File.separator + "Maps");
-			worldFolder = new File(NovaCoreGameEngine.getInstance().getRequestedGameDataDirectory().getAbsolutePath() + File.separator + getName() + File.separator + "Worlds");
+			mapFolder = new File(NovaCoreGameEngine.getInstance().getRequestedGameDataDirectory().getAbsolutePath() + File.separator + "CaptureTheFlag" + File.separator + "Maps");
+			worldFolder = new File(NovaCoreGameEngine.getInstance().getRequestedGameDataDirectory().getAbsolutePath() + File.separator + "CaptureTheFlag" + File.separator + "Worlds");
 		}
 
 		File mapOverrides = new File(this.getDataFolder().getPath() + File.separator + "map_overrides.json");
