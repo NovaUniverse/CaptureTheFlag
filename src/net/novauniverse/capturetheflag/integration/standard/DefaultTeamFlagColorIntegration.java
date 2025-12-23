bcee26a6540cdc49179525f9a603260eee3186c6
Anton Udén <udenanton@hotmail.com>
2025-12-23 18:00:47 +0100

    fix build errors

--- src/net/novauniverse/capturetheflag/integration/standard/DefaultTeamFlagColorIntegration.java
+++ src/net/novauniverse/capturetheflag/integration/standard/DefaultTeamFlagColorIntegration.java
@@ -1,59 +1,42 @@
 package net.novauniverse.capturetheflag.integration.standard;
 
 import org.bukkit.DyeColor;
-
+import net.md_5.bungee.api.ChatColor;
 import net.novauniverse.capturetheflag.integration.TeamFlagColorProvider;
 import net.zeeraa.novacore.spigot.teams.Team;
 
 public class DefaultTeamFlagColorIntegration implements TeamFlagColorProvider {
 	@Override
 	public DyeColor getTeamFlagColor(Team team) {
-		switch (team.getTeamColor()) {
-		case BLACK:
+		if (team.getTeamColor() == ChatColor.BLACK) {
 			return DyeColor.BLACK;
-
-		case DARK_BLUE:
-		case BLUE:
+		} else if (team.getTeamColor() == ChatColor.DARK_BLUE || team.getTeamColor() == ChatColor.BLUE) {
 			return DyeColor.BLUE;
-
-		case GOLD:
+		} else if (team.getTeamColor() == ChatColor.GOLD) {
 			return DyeColor.ORANGE;
-
-		case AQUA:
+		} else if (team.getTeamColor() == ChatColor.AQUA) {
 			return DyeColor.LIGHT_BLUE;
-
-		case DARK_AQUA:
+		} else if (team.getTeamColor() == ChatColor.DARK_AQUA) {
 			return DyeColor.CYAN;
-
-		case DARK_GRAY:
+		} else if (team.getTeamColor() == ChatColor.DARK_GRAY) {
 			return DyeColor.GRAY;
-
-		case DARK_GREEN:
+		} else if (team.getTeamColor() == ChatColor.DARK_GREEN) {
 			return DyeColor.GREEN;
-
-		case DARK_PURPLE:
+		} else if (team.getTeamColor() == ChatColor.DARK_PURPLE) {
 			return DyeColor.PURPLE;
-
-		case DARK_RED:
-		case RED:
+		} else if (team.getTeamColor() == ChatColor.DARK_RED || team.getTeamColor() == ChatColor.RED) {
 			return DyeColor.RED;
-
-		case GRAY:
+		} else if (team.getTeamColor() == ChatColor.GRAY) {
 			return DyeColor.SILVER;
-
-		case GREEN:
+		} else if (team.getTeamColor() == ChatColor.GREEN) {
 			return DyeColor.LIME;
-
-		case LIGHT_PURPLE:
+		} else if (team.getTeamColor() == ChatColor.LIGHT_PURPLE) {
 			return DyeColor.MAGENTA;
-
-		case WHITE:
+		} else if (team.getTeamColor() == ChatColor.WHITE) {
 			return DyeColor.WHITE;
-
-		case YELLOW:
+		} else if (team.getTeamColor() == ChatColor.YELLOW) {
 			return DyeColor.YELLOW;
-
-		default:
+		} else {
 			return DyeColor.WHITE;
 		}
 	}
